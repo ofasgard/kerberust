3784f7cfe4e9279a9585d05878db7ee8749f9766
Callum Murphy <callum.a.murphy.77@gmail.com>
2023-09-22 16:51:22 +0100

    add an error2string function

--- src/kdc_err.rs
+++ src/kdc_err.rs
@@ -35,3 +35,27 @@ pub fn parse_salt(krb_err : &KrbError) -> Result<String,()> {
 	
 	Err(())
 }
+
+pub fn format_error(krb_err : &KrbError) -> String {
+	let mut output = String::new();
+	
+	let error_code = format!("Error code: {}\n", &krb_err.error_code);
+	output.push_str(&error_code);
+	
+	if let Some(text) = &krb_err.e_text {
+		let error_text = format!("Error text: {}\n", text);
+		output.push_str(&error_text);
+	}
+	
+	if let Some(bytes) = &krb_err.e_data {
+		let error_data = format!("Error data: {:02X?}\n", bytes);
+		output.push_str(&error_data);
+	}
+	
+	if let Ok(salt) = parse_salt(&krb_err) {
+		let desired_salt = format!("Desired salt: {}\n", salt);
+		output.push_str(&desired_salt);
+	}
+	
+	output
+}
